81fc4e757e20bd146a85c2d83ebe1ac949bf0f61
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-05-27 08:30:43 +0000

    ejercicio final

--- src/app.js
+++ src/app.js
@@ -1,82 +1,42 @@
-/* eslint-disable */
-// import "bootstrap";
-// import "./style.css";
-
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
-
-// const excuseElement = document.querySelector("#excuse");
-
-// let who = ["The dog", "My grandma", "His turtle", "My bird"];
-// let action = ["ate", "peed", "crushed", "broke"];
-// let what = ["my homework", "the keys", "the car"];
-// let when = [
-//   "before the class",
-//   "right on time",
-//   "when I finished",
-//   "during my lunch",
-//   "while I was praying"
-// ];
-
-// function generarExcusa() {
-//   let whoAleatorio = who[Math.floor(Math.random() * who.length)];
-//   let actionAleatorio = action[Math.floor(Math.random() * action.length)];
-//   let whatAleatorio = what[Math.floor(Math.random() * what.length)];
-//   let whenAleatorio = when[Math.floor(Math.random() * when.length)];
-
-//   // Crear la excusa aleatoria
-
-//   let excusa =
-//     whoAleatorio +
-//     " " +
-//     actionAleatorio +
-//     " " +
-//     whatAleatorio +
-//     " " +
-//     whenAleatorio +
-//     ".";
-
-//   // Establecer la excusa en el elemento HTML con ID #excuse
-
-//   document.getElementById("excuse").innerHTML = excusa;
-//   //   const mainParagraph = document.getElementById("excuse");
-
-//   //   console.log(mainParagraph);
-// }
-
-// // Llamar a la función generarExcusa cuando se carga la página
-
-// generarExcusa();
-
 window.onload = function() {
-  //write your code here
+  const excuseElement = document.querySelector("#excuse");
 
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let div = document.getElementById("domain");
-  // Función para generar todas las combinaciones de nombres de dominio
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-  function generateDomainNames(pronoun, adj, noun) {
-    let domainNames = [];
+  function genererExcuse() {
+    let whoAleatory = who[Math.floor(Math.random() * who.length)];
+    let actionAleatory = action[Math.floor(Math.random() * action.length)];
+    let whatAleatory = what[Math.floor(Math.random() * what.length)];
+    let whenAleatory = when[Math.floor(Math.random() * when.length)];
 
-    for (var i = 0; i < pronoun.length; i++) {
-      for (var x = 0; x < adj.length; x++) {
-        for (var y = 0; y < noun.length; y++) {
-          let domainName = pronoun[i] + adj[x] + noun[y];
-          domainNames.push(domainName);
-        }
-      }
-    }
+    // Crear la excusa aleatoria
 
-    return domainNames;
+    let excuse =
+      whoAleatory +
+      " " +
+      actionAleatory +
+      " " +
+      whatAleatory +
+      " " +
+      whenAleatory +
+      ".";
+
+    // Establecer la excusa en el elemento HTML con ID #excuse
+
+    document.getElementById("excuse").innerHTML = excuse;
+    const mainParagraph = document.getElementById("excuse");
   }
 
-  var domainNames = generateDomainNames(pronoun, adj, noun);
-  for (var i = 0; i < domainNames.length; i++) {
-    div.innerHTML += `<p>${domainNames[i]}.com</p>`;
-    // console.log(domainNames[i] + ".com");
-  }
-
-  console.log("Hello Rigo from the console!");
+  genererExcuse();
+  let button = document.getElementById("genererButton");
+  button.addEventListener("click", genererExcuse);
 };
